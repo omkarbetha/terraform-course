0cf98db5131c40d11a2a33cca409e1def4d9040f
ZS-Omkar <omkar@zelarsoft.com>
2023-06-27 10:30:41 +0530

    subnet

--- vpc/main.tf
+++ vpc/main.tf
@@ -15,4 +15,11 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
+resource "aws_subnet" "public-subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
 
+  tags = {
+    Name = "terraform-public-subnet"
+  }
+}
