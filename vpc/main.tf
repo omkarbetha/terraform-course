d4fc79d8a8853f4b3d611f5de80a72d970f9c2a3
omkarbetha <omkareswar.b@yalamanchili.in>
2024-02-27 12:56:09 +0530

    Updated

--- vpc/main.tf
+++ vpc/main.tf
@@ -38,3 +38,25 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_route_table.id
 }
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "terraform-private-subnet"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "terraform-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_route_table.id
+}
