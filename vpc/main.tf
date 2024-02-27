2ccf7e1d42734e75b1a44e2137e4c77ca125356f
omkarbetha <omkareswar.b@yalamanchili.in>
2024-02-27 12:18:19 +0530

    updated

--- vpc/main.tf
+++ vpc/main.tf
@@ -15,11 +15,26 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
-resource "aws_subnet" "public-subnet" {
+resource "aws_subnet" "public_subnet" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
     Name = "terraform-public-subnet"
   }
+}
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags = {
+    Name = "terraform-public-rt"
+  }
+}
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
 }
